1164c51c38b2b2fdcac71a7f3c159674591633db
Ham Nghi Nguyen <hamnghi250699@gmail.com>
2020-10-19 16:57:59 -0400

    Added add and remove methods

--- src/com/company/Cart.java
+++ src/com/company/Cart.java
@@ -1,13 +1,20 @@
 package com.company;
 
 import java.util.ArrayList;
+import java.util.IllegalFormatException;
 import java.util.List;
 
-public class BookListCheckout {
-    private List<Book> checkoutList = new ArrayList<>();
-    private int bookItems;
-    public void addbookItem(Book item){
-        checkoutList.add(item);
+public class Cart extends ArrayList<Book>{
+    private ArrayList<Book> booklist;
+    public void addBook(Book book){
+        if (book.getBookStock()<0)
+            throw new IllegalArgumentException("Book is out of Stock");
+        booklist.add(book);
     }
-
+    public void removeBook(Book book){
+        for (Book book1 : booklist) {
+            if (book.getBookID().equals(book1.getBookID()))
+                booklist.remove(book);
+        }
+    }
 }
